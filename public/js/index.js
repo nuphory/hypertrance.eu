a8758c785cfe7c642fb599544fee3ca9a31de201
Mia <mia@mia.cx>
2021-12-22 05:58:36 +0100

    dynamic buttons & sections

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,73 @@
-document.getElementById("video-button").href = buttons.video.url
-    ? buttons.video.url
-    : "#";
-document.getElementById("album-button").href = buttons.video.url
-    ? buttons.album.url
-    : "#";
-document.getElementById("artbook-button").href = buttons.video.url
-    ? buttons.artbook.url
-    : "#";
+let root = document.documentElement;
 
-document.getElementById("video-text").innerHTML =
-    buttons.video.text.toUpperCase();
-document.getElementById("album-text").innerHTML =
-    buttons.album.text.toUpperCase();
-document.getElementById("artbook-text").innerHTML =
-    buttons.artbook.text.toUpperCase();
-loadImage();
+/**
+ * button generator
+ */
+Object.keys(buttons).forEach((id) => {
+    let button = document.createElement("div");
+    button.setAttribute("id", id + "-button");
+    button.setAttribute("class", "button");
+    button.innerHTML = "<p>" + buttons[id].text.toUpperCase() + "</p>";
 
-setTimeout(() => loadVideo(), 2000);
+    let anchor = document.createElement("a");
+    anchor.setAttribute("id", id + "-anchor");
+    anchor.setAttribute("href", buttons[id].url);
+    anchor.appendChild(button);
 
-async function loadImage() {
+    document.getElementById("buttons").appendChild(anchor);
+});
+
+let sectionsOffset = 30 * Object.keys(buttons).length;
+root.style.setProperty("--sections-offset", 238 + sectionsOffset + "px");
+
+/**
+ * section generator
+ */
+Object.keys(sections).forEach((id) => {
+    let section = document.createElement("div");
+    section.setAttribute("id", id + "-section");
+    section.setAttribute("class", "section");
+    document.getElementById("sections").appendChild(section);
+
+    let header = document.createElement("h2");
+    header.setAttribute("id", id + "-header");
+    header.setAttribute("class", "section-header");
+    header.innerText = sections[id].header.toUpperCase();
+
+    let content = document.createElement("span");
+    content.setAttribute("id", id + "-content");
+    content.setAttribute("class", "section-content");
+
+    document.getElementById(id + "-section").appendChild(header);
+    document.getElementById(id + "-section").appendChild(content);
+
+    Object.values(sections[id].content).forEach((p) => {
+        let paragraph = document.createElement("p");
+        paragraph.setAttribute("class", "section-paragraph");
+        paragraph.innerText = p;
+
+        document.getElementById(id + "-content").appendChild(paragraph);
+    });
+
+    let divider = document.createElement("div");
+    divider.setAttribute("class", "divider");
+    document.getElementById("sections").appendChild(divider);
+});
+
+Object.values(sections).forEach((section) => {
+    console.log(section);
+});
+
+loadBackgroundImage("img/background.jpg");
+setTimeout(() => loadBackgroundVideo("mp4/background.mp4"), 2000);
+
+async function loadBackgroundImage(path) {
     console.debug("adding background");
     document.getElementById("background").innerHTML =
-        '<img src="img/background.jpg"></img>';
+        '<img src="' + path + '"></img>';
 }
 
-async function loadVideo() {
+async function loadBackgroundVideo(path) {
     console.debug(document.getElementById("background"));
 
     let video = document.createElement("video");
@@ -35,7 +78,7 @@ async function loadVideo() {
 
     video.setAttribute("id", "background-video-video");
 
-    video.innerHTML = '<source src="mp4/background.mp4" type="video/mp4">';
+    video.innerHTML = '<source src="' + path + '" type="video/mp4">';
 
     document.getElementById("background").appendChild(video);
 }
