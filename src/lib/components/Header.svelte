2231473f5a8e2f02a08562bd917eaeb9c1b11d65
mia-cx <mia@mia.cx>
2023-03-12 06:18:26 +0100

    basic header, empty footer, basic layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { name } from '$lib/identity';
+</script>
+
+<header class="">
+	<nav class="flex flex-row justify-between items-center h-[4rem] max-w-7xl m-auto p-4">
+		<h2><a href="/" class="reset hover:scale-105 origin-left">{name.toUpperCase()}</a></h2>
+		<ul class="hidden">
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+        // Imports
+
+        // Components
+        import Footer from "$lib/components/Footer.svelte";
+        import Header from "$lib/components/Header.svelte";
+
         // Styles
         import "$lib/styles/app.scss";
+        
+        import '@fontsource/playfair-display-sc/';
 </script>
+
+<Header />
+<slot />
+<Footer />
