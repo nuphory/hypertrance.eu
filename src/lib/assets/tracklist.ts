c059276cd5bc8845e39bfa1f4e0dfac0fee3daf1
mia-cx <mia@mia.cx>
2023-03-12 06:16:33 +0100

    hypertrance 2 tracklist

---
+++ src/lib/assets/tracklist.ts
@@ -0,0 +1,234 @@
+export const tracklist = [
+	{
+		title: 'HYPERTRANCE',
+		artists: [
+			{
+				name: 'nuphory',
+				link: 'https://soundcloud.com/nuphory'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'everafter',
+		artists: [
+			{
+				name: 'wyste',
+				link: 'https://soundcloud.com/wyste'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'infinity',
+		artists: [
+			{
+				name: 'aekae',
+				link: 'https://soundcloud.com/aekaemusic'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'digital eternity',
+		artists: [
+			{
+				name: 'yuuni',
+				link: 'https://soundcloud.com/yuunimoosic'
+			},
+			{
+				name: 'aekae',
+				link: 'https://soundcloud.com/aekaemusic'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Myria Exaxis',
+		artists: [
+			{
+				name: 'Type R',
+				link: 'https://soundcloud.com/hiimtype_r'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'EXOPLANET // HYPERTRANCE',
+		artists: [
+			{
+				name: 'nuphory',
+				link: 'https://soundcloud.com/nuphory'
+			},
+			{
+				name: 'Luna Lenta',
+				link: 'https://soundcloud.com/lunalenta'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Carry All Before One v2',
+		artists: [
+			{
+				name: 'Lure Rabbit',
+				link: 'https://soundcloud.com/lurerabbit'
+			},
+			{
+				name: 'NaiMeng',
+				link: ''
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Ghost Wire',
+		artists: [
+			{
+				name: 'crayvxn',
+				link: 'https://soundcloud.com/crayvxn'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Infinite Evolution',
+		artists: [
+			{
+				name: 'Neurolink',
+				link: 'https://soundcloud.com/neurolinkmusic'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'through the forest',
+		artists: [
+			{
+				name: 'voxkai',
+				link: 'https://soundcloud.com/voxkai'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Dryftcurrent',
+		artists: [
+			{
+				name: 'Kassio Project',
+				link: 'https://soundcloud.com/kassio-971255094'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Wait For Me',
+		artists: [
+			{
+				name: 'D4VT0R',
+				link: 'https://soundcloud.com/d4vt0rmusic'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'REJECTEDBYHYPERTRANCE',
+		artists: [
+			{
+				name: 'still crisp',
+				link: 'https://soundcloud.com/stillcrisp'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'ask the universe',
+		artists: [
+			{
+				name: 'Gasker',
+				link: 'https://soundcloud.com/gasker'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'SSR05',
+		artists: [
+			{
+				name: 'MOSHII LIGHTZ',
+				link: 'https://soundcloud.com/moshii-lightz'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'I NEED',
+		artists: [
+			{
+				name: 'C-ZER',
+				link: 'https://soundcloud.com/thisisczer'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'STEP10',
+		artists: [
+			{
+				name: 'SAM WAITIN',
+				link: 'https://soundcloud.com/samwaitin'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Moon Shine',
+		artists: [
+			{
+				name: 'Lunanescence',
+				link: 'https://soundcloud.com/lunanescence'
+			},
+			{
+				name: 'Luna Lenta',
+
+				link: 'https://soundcloud.com/lunalenta'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'juju stomp',
+		artists: [
+			{
+				name: 'disctr4k',
+				link: 'https://soundcloud.com/disctr4k'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'Twilight',
+		artists: [
+			{
+				name: 'Ausa',
+
+				link: 'https://soundcloud.com/ausamusicofficial'
+			}
+		],
+		link: ''
+	},
+	{
+		title: 'soul vertex',
+		artists: [
+			{
+				name: 'Noah B',
+				link: 'https://soundcloud.com/thisisnoahb'
+			}
+		],
+		link: ''
+	}
+];
+
+export const visualArtists = []
+
+export default tracklist;
