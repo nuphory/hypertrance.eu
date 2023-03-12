a28402a90bc4abe55ac130d1daebda364994e33b
mia-cx <mia@mia.cx>
2023-03-12 06:30:34 +0100

    build to dist/ & ignore dist/

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		outDir: 'dist',
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
