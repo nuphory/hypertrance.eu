e094127b1f72f99ed8728d3f442ebeaa65ce2cf0
mia-cx <mia@mia.cx>
2023-03-12 06:16:24 +0100

    identity of the site (imporant)

---
+++ src/lib/identity.ts
@@ -0,0 +1,17 @@
+export const name = 'Hypertrance';
+export const tagline = 'Hypertrance';
+export const description = '';
+
+export const url = 'https://hypertrance.eu';
+
+export const keywords = [
+        ''
+];
+
+export const id = {
+        name,
+        tagline,
+        description,
+        url,
+        keywords
+}
