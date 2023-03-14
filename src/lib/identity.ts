8b482f2b715dad4305d62430e2ec239772e5844c
mia-cx <mia@mia.cx>
2023-03-14 12:22:18 +0100

    formatting + tagline + id on html

--- src/lib/identity.ts
+++ src/lib/identity.ts
@@ -1,17 +1,15 @@
-export const name = 'Hypertrance';
-export const tagline = 'Hypertrance';
+export const name = 'hypertrance';
+export const tagline = 'the definitive culmination of neoy2k music and art';
 export const description = '';
 
 export const url = 'https://hypertrance.eu';
 
-export const keywords = [
-        ''
-];
+export const keywords = [''];
 
 export const id = {
-        name,
-        tagline,
-        description,
-        url,
-        keywords
-}
+	name,
+	tagline,
+	description,
+	url,
+	keywords
+};
