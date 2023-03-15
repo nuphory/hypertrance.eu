29ab2422433d7b434b95fcbc4d9caaa19c61f966
mia-cx <mia@mia.cx>
2023-03-15 21:39:25 +0100

    right i forgot i switched this buddy to prod

--- src/lib/identity.ts
+++ src/lib/identity.ts
@@ -2,7 +2,7 @@ export const name = 'hypertrance';
 export const tagline = 'the definitive culmination of neoy2k music and art';
 export const description = 'lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
-export const url = 'https://v2.hypertrance.pages.dev';
+export const url = 'https://hypertrance.pages.dev';
 // export const url = 'https://hypertrance.eu'; // TODO switch to this on launch
 
 export const keywords = [''];
