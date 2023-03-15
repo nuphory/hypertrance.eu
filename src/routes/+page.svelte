a48d8ea3b9388002655c9bb8553e51a4550a3931
mia-cx <mia@mia.cx>
2023-03-15 04:37:23 +0100

    use clip path in stead of blend mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,18 +52,17 @@
 		/> -->
 	</figure>
 
-	<section class="max-w-none font-comsat-navy mix-blend-lighten px-0" id="hero">
-		<div class="mt-24 text-white uppercase ">
-			<div class="origin-bottom rotate-x-6 sm:-mt-14 md:-mt-[5rem] lg:-mt-[7rem]">
+	<section class="max-w-none font-comsat-navy px-0" id="hero">
+		<div class="mt-24 perspective-40 text-white uppercase ">
+			<div class="origin-bottom rotate-x-6 sm:-mt-14 md:-mt-[5rem] lg:-mt-[7rem] ">
 				<div class="container relative mx-auto px-0 font-extralight">
 					<span
-						class="block translate-y-[2.5%] -mr-8 lg:-mr-4 text-[16rem] md:text-[24rem] lg:text-[32rem] leading-[16rem] md:leading-[24rem] lg:leading-[32rem] tracking-[3rem] md:tracking-[4rem] lg:tracking-[4.5rem] text-right text-white [-webkit-text-fill-color:black] [-webkit-text-stroke-width:1px] select-none pointer-events-none"
+						class="cutout block translate-y-[2.5%] -mr-8 lg:-mr-4 text-[16rem] md:text-[24rem] lg:text-[32rem] leading-[16rem] md:leading-[24rem] lg:leading-[32rem] tracking-[3rem] md:tracking-[4rem] lg:tracking-[4.5rem] text-right text-white [-webkit-text-fill-color:transparent] [-webkit-text-stroke-width:1px] select-none pointer-events-none"
 						>02</span
 					>
-					<div class="absolute top-1/2 -translate-y-1h w-full h-1/4 bg-black" />
 				</div>
 				<div
-					class="container relative flex justify-between items-start max-w-none mx-0 px-0 -mt-[9rem] mb-[9rem] md:-mt-[13.25rem] md:mb-[13.25rem] lg:-mt-[18rem] lg:mb-[18rem]"
+					class="container relative flex justify-between items-start max-w-none mx-0 px-0 -mt-[9.5rem] mb-[9.5rem] md:-mt-[14rem] md:mb-[14rem] lg:-mt-[19rem] lg:mb-[19rem]"
 				>
 					<div
 						class="grow h-[0.2rem] md:h-[0.3rem] lg:h-[0.4rem] -ml-16 md:-ml-32 lg:-ml-[32rem] -mr-4 md:-mr-8 bg-white"
@@ -125,102 +124,101 @@
 		</div>
 	</section>
 
-
 	<div id="content" class="from-neutral via-neutral bg-gradient-to-t">
-                <article class="container sm:-mb-24 grid lg:grid-cols-2 gap-8 auto-rows-max">
-                        <section class="p-0 -mb-8 lg:col-start-2">
-                                <iframe
-                                        allow="fullscreen"
-                                        allowfullscreen
-                                        class="aspect-video "
-                                        frameborder="0"
-                                        title="Hypertrance 2"
-                                        src="https://www.youtube-nocookie.com/embed/nONsDtHZk5Y?&controls=1&showinfo=1&start=9"
-                                />
-                        </section>
-                        <section class="lg:row-start-1 row-span-2" id="about">
-                                <h2>what is hypertrance?</h2>
-                                <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam placeat hic quisquam culpa
-                                        omnis non dicta sequi, dolore possimus, eligendi officiis nostrum. Placeat provident
-                                        deleniti culpa totam recusandae, voluptates aperiam!
-                                </p>
-                                <p>
-                                        Quae ducimus a dolore ex molestiae cupiditate ipsum fuga tempore, quam quasi voluptate,
-                                        perferendis et enim nam fugit eius harum! Non aspernatur explicabo tenetur similique
-                                        obcaecati. Error rerum sunt ipsa.
-                                </p>
-                                <p>
-                                        Quaerat illo repudiandae sit placeat dolor. Quos, magni consequatur, autem quo facere
-                                        assumenda dolorem ducimus modi illo voluptate ad praesentium repudiandae, esse aperiam
-                                        labore accusamus consequuntur nostrum. Temporibus, molestias pariatur!
-                                </p>
-                                <p>
-                                        Natus doloribus eius fugit quis expedita nihil hic excepturi quam dolorem aut quos numquam
-                                        sint explicabo animi, aspernatur commodi amet quia, repellendus similique quidem? Sapiente
-                                        quasi quos accusamus quia praesentium!
-                                </p>
-                                <p>
-                                        Vero sit omnis molestiae dolore temporibus! Animi praesentium eligendi veritatis, eaque
-                                        voluptatem consequuntur laborum in laboriosam saepe veniam neque itaque officiis rerum est?
-                                        Sapiente tempore dolores obcaecati nobis suscipit odio!
-                                </p>
-                        </section>
-                        <section class="" id="tracklist">
-                                <h2>tracklist</h2>
-                                <ol class="list-decimal max-w-lg">
-                                        {#each tracklist as track}
-                                                <li class="ml-8">
-                                                        <div class="flex gap-1">
-                                                                <div class="tracklist__artists flex-1 text-left">
-                                                                        {#each track.artists as artist, i}
-                                                                                {#if artist}
-                                                                                        {#if i !== 0}
-                                                                                                <span class="tracklist__combinator">&nbsp&</span>
-                                                                                        {/if}
-                                                                                        {#if artist.links.length > 0}
-                                                                                                <a
-                                                                                                        class="tracklist__artist"
-                                                                                                        href={(
-                                                                                                                artist.links.find((link) => link.name === 'bandcamp') ??
-                                                                                                                artist.links.find((link) => link.name === 'spotify') ??
-                                                                                                                artist.links.find((link) => link.name === 'soundcloud') ??
-                                                                                                                artist.links[0]
-                                                                                                        ).url}
-                                                                                                >
-                                                                                                        {artist.name}
-                                                                                                </a>
-                                                                                        {:else}
-                                                                                                <span class="tracklist__artist">{artist.name}</span>
-                                                                                        {/if}
-                                                                                {/if}
-                                                                        {/each}
-                                                                </div>
-                                                                <span class="tracklist__separator">-</span>
-                                                                <span class="tracklist__title flex-1">
-                                                                        {#if track.links.length > 0}
-                                                                                <a
-                                                                                        class="tracklist__artist"
-                                                                                        href={(
-                                                                                                track.links.find((link) => link.name === 'bandcamp') ??
-                                                                                                track.links.find((link) => link.name === 'spotify') ??
-                                                                                                track.links.find((link) => link.name === 'soundcloud') ??
-                                                                                                track.links[0]
-                                                                                        ).url}
-                                                                                >
-                                                                                        {track.title}
-                                                                                </a>
-                                                                        {:else}
-                                                                                <span class="tracklist__artist">{track.title}</span>
-                                                                        {/if}
-                                                                </span>
-                                                        </div>
-                                                </li>
-                                        {/each}
-                                </ol>
-                        </section>
-                </article>
-        </div>
+		<article class="container sm:-mb-24 grid lg:grid-cols-2 gap-8 auto-rows-max">
+			<section class="p-0 -mb-8 lg:col-start-2">
+				<iframe
+					allow="fullscreen"
+					allowfullscreen
+					class="aspect-video "
+					frameborder="0"
+					title="Hypertrance 2"
+					src="https://www.youtube-nocookie.com/embed/nONsDtHZk5Y?&controls=1&showinfo=1&start=9"
+				/>
+			</section>
+			<section class="lg:row-start-1 row-span-2" id="about">
+				<h2>what is hypertrance?</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam placeat hic quisquam
+					culpa omnis non dicta sequi, dolore possimus, eligendi officiis nostrum. Placeat provident
+					deleniti culpa totam recusandae, voluptates aperiam!
+				</p>
+				<p>
+					Quae ducimus a dolore ex molestiae cupiditate ipsum fuga tempore, quam quasi voluptate,
+					perferendis et enim nam fugit eius harum! Non aspernatur explicabo tenetur similique
+					obcaecati. Error rerum sunt ipsa.
+				</p>
+				<p>
+					Quaerat illo repudiandae sit placeat dolor. Quos, magni consequatur, autem quo facere
+					assumenda dolorem ducimus modi illo voluptate ad praesentium repudiandae, esse aperiam
+					labore accusamus consequuntur nostrum. Temporibus, molestias pariatur!
+				</p>
+				<p>
+					Natus doloribus eius fugit quis expedita nihil hic excepturi quam dolorem aut quos numquam
+					sint explicabo animi, aspernatur commodi amet quia, repellendus similique quidem? Sapiente
+					quasi quos accusamus quia praesentium!
+				</p>
+				<p>
+					Vero sit omnis molestiae dolore temporibus! Animi praesentium eligendi veritatis, eaque
+					voluptatem consequuntur laborum in laboriosam saepe veniam neque itaque officiis rerum
+					est? Sapiente tempore dolores obcaecati nobis suscipit odio!
+				</p>
+			</section>
+			<section class="" id="tracklist">
+				<h2>tracklist</h2>
+				<ol class="list-decimal max-w-lg">
+					{#each tracklist as track}
+						<li class="ml-8">
+							<div class="flex gap-1">
+								<div class="tracklist__artists flex-1 text-left">
+									{#each track.artists as artist, i}
+										{#if artist}
+											{#if i !== 0}
+												<span class="tracklist__combinator">&nbsp&</span>
+											{/if}
+											{#if artist.links.length > 0}
+												<a
+													class="tracklist__artist"
+													href={(
+														artist.links.find((link) => link.name === 'bandcamp') ??
+														artist.links.find((link) => link.name === 'spotify') ??
+														artist.links.find((link) => link.name === 'soundcloud') ??
+														artist.links[0]
+													).url}
+												>
+													{artist.name}
+												</a>
+											{:else}
+												<span class="tracklist__artist">{artist.name}</span>
+											{/if}
+										{/if}
+									{/each}
+								</div>
+								<span class="tracklist__separator">-</span>
+								<span class="tracklist__title flex-1">
+									{#if track.links.length > 0}
+										<a
+											class="tracklist__artist"
+											href={(
+												track.links.find((link) => link.name === 'bandcamp') ??
+												track.links.find((link) => link.name === 'spotify') ??
+												track.links.find((link) => link.name === 'soundcloud') ??
+												track.links[0]
+											).url}
+										>
+											{track.title}
+										</a>
+									{:else}
+										<span class="tracklist__artist">{track.title}</span>
+									{/if}
+								</span>
+							</div>
+						</li>
+					{/each}
+				</ol>
+			</section>
+		</article>
+	</div>
 	<section class="sticky bottom-0 max-w-none sm:h-0 sm:translate-y-full font-comsat-navy">
 		<ul class="container flex justify-between xs:justify-end items-start gap-8">
 			<li class="max-xs:flex-1">
@@ -245,3 +243,10 @@
 		</ul>
 	</section>
 </main>
+
+<style lang="scss">
+	span.cutout {
+		clip-path: polygon(100% 33%, 0 33%, 0 0, 100% 0%, 100% 100%, 0 100%, 0 60%, 100% 60%);
+		
+	}
+</style>
