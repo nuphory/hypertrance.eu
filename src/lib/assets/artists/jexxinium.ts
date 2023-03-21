8fa9410d00815ac3e46f3be0511e87c38209a364
mia-cx <mia@mia.cx>
2023-03-21 19:51:30 +0100

    formatting

--- src/lib/assets/artists/jexxinium.ts
+++ src/lib/assets/artists/jexxinium.ts
@@ -2,27 +2,26 @@ import { ArtistBuilder } from '$lib/metadata/artist';
 import { LinkBuilder, Platform } from '$lib/metadata/link';
 
 // TODO: Add links
-const JEXXINIUM = 
-    new ArtistBuilder()
-        .setName('jexxinium')
-        .addLink(
-            new LinkBuilder()
-                .setPlatform(Platform.WEBSITE)
-                .setUrl('https://thejexxinium.neocities.org/')
-                .create()
-        )
-        .addLink(
-            new LinkBuilder()
-                .setPlatform(Platform.DEVIANT_ART)
-                .setUrl('https://www.deviantart.com/thejexxinium')
-                .create()
-        )
-        .addLink(
-            new LinkBuilder()
-                .setPlatform(Platform.SPACEHEY)
-                .setUrl('https://www.deviantart.com/thejexxinium')
-                .create()
-        )
-        .create();
+const JEXXINIUM = new ArtistBuilder()
+	.setName('jexxinium')
+	.addLink(
+		new LinkBuilder()
+			.setPlatform(Platform.WEBSITE)
+			.setUrl('https://thejexxinium.neocities.org/')
+			.create()
+	)
+	.addLink(
+		new LinkBuilder()
+			.setPlatform(Platform.DEVIANT_ART)
+			.setUrl('https://www.deviantart.com/thejexxinium')
+			.create()
+	)
+	.addLink(
+		new LinkBuilder()
+			.setPlatform(Platform.SPACEHEY)
+			.setUrl('https://www.deviantart.com/thejexxinium')
+			.create()
+	)
+	.create();
 
 export default JEXXINIUM;
