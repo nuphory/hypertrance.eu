c944139a7a021a3395bb148a65e5e36d342dc16f
Mia <mia@mia.cx>
2023-07-31 17:58:45 +0200

    three-js-test? idk

---
+++ src/routes/three-js-test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { createScene } from './scene';
+
+	let canvas;
+
+	onMount(() => {
+		createScene(canvas);
+	});
+</script>
+
+<canvas bind:this={canvas} />
