069b3bc40762166af5b9171ee6b65c0795858149
Djkáťo <radovan.katrencik@gmail.com>
2023-08-04 16:56:06 +0200

    force mp3s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,9 +34,9 @@
 	onMount(() => {
 		if (browser) {
 			const audio = document.createElement('audio');
-			if (/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) {
-				db.songs.forEach((s) => s.url.href.replace('.ogg', '.mp3'));
-			}
+			db.songs.forEach((s) => (s.url = new URL(s.url.href.replace('.ogg', '.mp3'))));
+			// if (/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) {
+			// }
 			player = new EuterpeBuilder(audio, db).build();
 			document.body.appendChild(audio);
 
