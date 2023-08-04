b186dc6f026be897400ed63aed43738a947f9145
Djkáťo <radovan.katrencik@gmail.com>
2023-08-04 18:58:59 +0200

    Component out hero, add details section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
 	import THREED from '../lib/components/landing/THREED.svelte';
 	import { browser } from '$app/environment';
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { db } from '$lib/scripts/music/db';
-	import Money from '$src/lib/components/Money.svelte';
-	import { createCart } from '$src/lib/utils/shopify';
 	import { Euterpe, EuterpeBuilder } from '@euterpe.js/euterpe';
 	import { onMount } from 'svelte';
 	import Sample from '../lib/components/landing/Sample.svelte';
 	import Volume from '../lib/components/landing/Volume.svelte';
+	import Stats from '../lib/components/landing/Stats.svelte';
+	import Hero from '../lib/components/landing/Hero.svelte';
 	export let data;
-
-	const { product } = data;
-
-	const selectedVariant = product.variants.nodes[0];
 
 	//temp description
 	db.collections.forEach((col) => {
@@ -58,83 +53,36 @@
 			});
 		}
 	});
-
-	async function directBuy() {
-		if (!product) throw Error('Product not found');
-		const cart = await createCart(selectedVariant.id, 1);
-		if (!cart) throw Error('Cart creation failed');
-		goto(cart.checkoutUrl);
-	}
 </script>
 
 <main id="landing-page" class="font-michroma text-primary bg-primary">
 	<Volume {player} />
-	<section class="w-full h-[110vh] relative text-bg-primary">
+	<section id="about" class="w-full h-[110vh] relative text-bg-primary">
 		<THREED />
-		<div
-			class="z-10 absolute w-full md:w-auto right-0 bottom-[30vh] md:right-[15vw] md:bottom-[30vh] md:skew-x-[25deg] bg-primary-side md:pl-4"
-		>
-			<div
-				class="flex flex-col bg-text-primary text-black md:[&>*]:-skew-x-[25deg] px-20 py-4 pb-8 gap-8"
-			>
-				<h1 class="before:content-[''] mx-auto md:-mb-3">
-					<span class="block text-4xl w-full -mb-2">HyPERTRANCE</span>
-					<span class="pl-24 block text-4xl">SAMPLEPACK</span>
-				</h1>
-				<div class="flex gap-3 justify-center items-end -mt-1">
-					<Money
-						price={selectedVariant.price}
-						showCurrency={true}
-						class="p-2 ml-1 px-[2.4rem] w-fit skew-x-[25deg] [&>*]:-skew-x-[25deg] font-michroma before:content-[''] bg-primary-side ring-1 ring-primary text-primary"
-					>
-						<span class="text-xs whitespace-nowrap overflow-visible -ml-2 mb-1 w-0"
-							>introductory price</span
-						>
-					</Money>
-					{#if selectedVariant.compareAtPrice !== null && selectedVariant.compareAtPrice !== selectedVariant.price}
-						<Money
-							price={selectedVariant.compareAtPrice}
-							compareAtPrice={true}
-							showCurrency={true}
-							class=" -ml-3 p-2 px-4 w-fit  skew-x-[25deg] [&>*]:-skew-x-[25deg] ring-1 ring-primary font-michroma  decoration-black"
-						/>
-					{/if}
-				</div>
-				<div class="text-2xl flex justify-between">
-					<button
-						class="hyper-button !px-8 button-primary-inverse skew-x-[25deg] [&>*]:-skew-x-[25deg]"
-						on:click={directBuy}
-					>
-						<span class="inline-block">buy now</span>
-					</button>
-					<!-- <div class="bg-primary skew-x-[25deg] py-1 px-8 m-2">
-						<a
-                                                        data-sveltekit-reload
-							class="-skew-x-[25deg] before:content-['>_']"
-							href="/app/store/product/hypertrance-samplepack">buy now</a
-						>
-					</div> -->
-					<a
-						href="/app/store/product/hypertrance-samplepack"
-						data-sveltekit-reload
-						class="block text-bg-primary hover:text-black active:text-black py-1 px-2 m-2"
-						>learn more</a
-					>
-				</div>
+		<Hero {data} />
+	</section>
+	<section class="my-4 px-24">
+		<div class="grid grid-cols-1 gap-8 w-full">
+			<h2 class="text-5xl before:[content:''] text-center text-[var(--color-content-emphasis)]">
+				EXPERIENCE THE HyPE OR SMT
+			</h2>
+			<Stats />
+			<p class="my-4 indent-8 text-justify">
+				This would be another good spot to describe the product more, probably explain what it even
+				is and sound hype througout the whole thing. This could be a multi-line long description,
+				although not sure about that since we don't have a single serif font in our library yet. We
+				will cross that bridge once we get there, for now we don' even know if this paragraph will
+				be here. Aight not sure what more to say so let's end it here c:
+			</p>
+			<div>
+				<h3 class="text-4xl mt-8 text-center text-[var(--color-content-emphasis)]">DEMOS</h3>
+				{#each demos.songs as song, i}
+					<Sample {is_playing} {player} {playing_song_id} song={song.get(db)} />
+				{/each}
 			</div>
-			<a href="#about" class="block text-4xl -mb-7 py-4 text-center font-suissnord"
-				><span class="inline-block -skew-x-[25deg]">🢗</span></a
-			>
 		</div>
 	</section>
-	<section id="about" class="">
-		<div class="grid grid-cols-1 gap-2 px-12">
-			{#each demos.songs as song, i}
-				<Sample {is_playing} {player} {playing_song_id} song={song.get(db)} />
-			{/each}
-		</div>
-	</section>
-	<section class="grid grid-cols-1 grid-rows-4 bg-primary">
+	<section class="mt-24 grid grid-cols-1 grid-rows-4 bg-primary">
 		{#each collections as collection, i}
 			<div
 				class="w-full h-full p-12 py-6 grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-6 relative"
@@ -148,7 +96,7 @@
 					>
 						{collection.name}
 					</h2>
-					<p class="mt-6 py-4 mx-8">
+					<p class="mt-6 py-4 mx-8 text-justify">
 						{collection.metadata[0]}
 					</p>
 					<div class="mt-6 grid grid-cols-1 gap-2">
@@ -168,6 +116,9 @@
 <style lang="scss">
 	:global(html) {
 		font-size: clamp(0.5rem, calc(0.6rem + 0.3vw), 2rem);
+	}
+	section:not(:first-of-type) {
+		@apply mx-auto lg:max-w-[1920px];
 	}
 
 	:global(:root) {
