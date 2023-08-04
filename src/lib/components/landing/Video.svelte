f51f155653dc99c5587523d205a87501c16a65d8
Djkáťo <radovan.katrencik@gmail.com>
2023-08-04 19:46:30 +0200

    upgrades to euterpe.

--- src/lib/components/landing/Video.svelte
+++ src/lib/components/landing/Video.svelte
@@ -9,21 +9,23 @@
 	let video_element: HTMLVideoElement;
 	let src = '';
 	onMount(() => {
-		if (browser) {
-			const width = video_element.clientWidth;
-			console.log(width);
-			src_set.sort((a, b) => {
-				if (a.breakpoint == b.breakpoint) return 0;
-				if (a.breakpoint > b.breakpoint) return 1;
-				else return -1;
-			});
-			let i = 0;
-			while (true) {
-				if (src_set[i].breakpoint > width) break;
-				i++;
+		setTimeout(() => {
+			if (browser) {
+				const width = video_element.clientWidth;
+				console.log(width);
+				src_set.sort((a, b) => {
+					if (a.breakpoint == b.breakpoint) return 0;
+					if (a.breakpoint > b.breakpoint) return 1;
+					else return -1;
+				});
+				let i = 0;
+				while (true) {
+					if (src_set[i].breakpoint > width) break;
+					i++;
+				}
+				src = src_set[0].path;
 			}
-			src = src_set[0].path;
-		}
+		}, 100);
 	});
 </script>
 
@@ -36,5 +38,5 @@
 	muted={muted || null}
 	loop={loop || null}
 >
-	<source src={src_set[2].path} type="video/webm" />
+	<source src={src_set[3].path} type="video/webm" />
 </video>
