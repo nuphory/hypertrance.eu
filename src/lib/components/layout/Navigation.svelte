15ad91130234d899bc4a9016465b06105672393c
Mia <mia@mia.cx>
2023-08-09 19:58:30 +0200

    disable links that go nowhere

--- src/lib/components/layout/Navigation.svelte
+++ src/lib/components/layout/Navigation.svelte
@@ -1,8 +1,8 @@
 <nav class="flex gap-12 {$$restProps.class}">
 	<ul style="display: contents;">
-		<li><a href="/">Home</a></li>
-		<li><a href="/app/store/product/hypertrance-samplepack">Store</a></li>
-		<li><a title="Coming soon" href="#" disabled>Catalog</a></li>
-		<li><a title="Coming soon" href="#" disabled>Wiki</a></li>
+		<li><a href="/home">Home</a></li>
+		<li><a href="/store/product/hypertrance-samplepack">Store</a></li>
+		<li><a title="Coming soon" href="/catalog" disabled>Catalog</a></li>
+		<li><a title="Coming soon" href="/wiki" disabled>Wiki</a></li>
 	</ul>
 </nav>
