c79f4dc9d7822247c2a9915446b760833fc0bd43
Djkáťo <radovan.katrencik@gmail.com>
2023-08-09 19:19:18 +0200

    Euterpe upgrade, fix volume n samples

--- src/lib/components/pages/samplepack-promo/Volume.svelte
+++ src/lib/components/pages/samplepack-promo/Volume.svelte
@@ -7,14 +7,18 @@
 
 <div
 	class=" hidden md:grid fixed left-24 bottom-24 w-16 h-16 bg-text-primary z-10 rounded-full p-2
-    grid-cols-1 grid-flow-row [transition-duration:_100ms] transition-all
+    grid-cols-1 grid-flow-row [transition-duration:_100ms] transition-all shadow-[var(--color-bg-side)] shadow-[0_0_3rem_5px]
+	place-items-end
     {is_hiding
-		? 'h-16 grid-rows-[1fr_3rem] hover:scale-90 active:scale-105 active:brightness-105 shadow-[var(--color-bg-side)] shadow-[0_0_3rem_5px] hover:brightness-75'
+		? 'h-16 grid-rows-[1fr_3rem] hover:scale-90 active:scale-105 active:brightness-105  hover:brightness-75'
 		: 'h-[20rem] grid-rows-[1fr_3rem]'}
     "
 >
-	<div class=" mx-auto [transition-duration:_100ms] {is_hiding ? 'w-0 h-0 ' : 'w-4 h-full'}">
+	<div
+		class=" mx-auto [transition-duration:_100ms] ml-6 {is_hiding ? 'w-0 h-0 ' : 'w-[15rem] h-4'}"
+	>
 		<input
+			on:mouseup={(e) => player.change_volume(e.target.valueAsNumber)}
 			class="volumeSlider {is_hiding ? 'hidden' : 'block'}"
 			type="range"
 			min="0"
@@ -43,19 +47,20 @@
 			@apply hidden;
 		}
 	}
+	//  [-webkit-appearance:_slider-vertical] [writing-mode:_bt-lr] if custom track ain't workie
 	.volumeSlider {
-		@apply appearance-none bg-transparent h-full w-4 relative m-0 p-0 py-2 border-none cursor-pointer;
+		@apply appearance-none [transform:rotate(270deg)] origin-left bg-transparent w-full h-4 relative m-0 p-0 py-2 border-none cursor-pointer -mb-[14rem];
 	}
 	.volumeSlider::-webkit-slider-runnable-track {
-		@apply bg-[var(--color-bg-side)] rounded-full h-full;
+		@apply bg-[var(--color-bg-side)] rounded-full w-full h-4 appearance-none;
 	}
 
 	.volumeSlider::-moz-range-track {
-		@apply bg-[var(--color-bg-side)] rounded-full h-full;
+		@apply bg-[var(--color-bg-side)] rounded-full w-full h-4 appearance-none;
 	}
 
 	.volumeSlider::-ms-track {
-		@apply bg-[var(--color-bg-side)] rounded-full h-full;
+		@apply bg-[var(--color-bg-side)] rounded-full w-full h-4 appearance-none;
 	}
 
 	.volumeSlider::-moz-range-thumb {
