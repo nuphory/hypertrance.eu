646cade02c50af0ed6f0841d27905ebb0bea2303
Mia <mia@mia.cx>
2023-08-10 15:29:57 +0200

    lets try this one more time...

--- src/routes/(main)/store/product/[handle]/+page.server.ts
+++ src/routes/(main)/store/product/[handle]/+page.server.ts
@@ -1,17 +1,17 @@
-export const prerender = true;
 export const ssr = true;
+export const csr = false;
 
 // sveltekit load
-import { getProductByHandle } from '$lib/utils/shopify/product';
+import { getProductByHandle } from '$lib/utils/shopify/product.server';
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params, url }) {
+export async function load({ params, url, getClientAddress }) {
 	const { handle } = params;
 
 	const { href } = url;
 
-	const product = await getProductByHandle({ handle, selectedOptions: [] });
+	const product = await getProductByHandle({ handle }, getClientAddress());
 
 	if (!product) {
 		throw error(404, { message: 'Product not found' });
--- src/routes/(main)/store/product/hypertrance-samplepack/+page.server.ts
+++ src/routes/(main)/store/product/hypertrance-samplepack/+page.server.ts
@@ -1,17 +1,17 @@
-export const prerender = true;
 export const ssr = true;
+export const csr = false;
 
 // sveltekit load
-import { getProductByHandle } from '$lib/utils/shopify/product';
+import { getProductByHandle } from '$lib/utils/shopify/product.server';
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ url }) {
+export async function load({ url, getClientAddress }) {
 	const handle = 'hypertrance-samplepack';
 
 	const { href } = url;
 
-	const product = await getProductByHandle({ handle, selectedOptions: [] });
+	const product = await getProductByHandle({ handle }, getClientAddress());
 
 	if (!product) {
 		throw error(404, { message: 'Product not found' });
