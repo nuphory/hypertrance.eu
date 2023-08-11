1008559362c81d7a0029577d664044efaddee321
Mia <mia@mia.cx>
2023-08-11 21:54:40 +0200

    overflow fix in cart

--- src/lib/components/layout/CartDrawer.svelte
+++ src/lib/components/layout/CartDrawer.svelte
@@ -77,7 +77,7 @@
 			<div class="border-t my-2" />
 
 			<!-- Cart Contents -->
-			<div class="flex-1 overflow-scroll">
+			<div class="flex-1 overflow-y-scroll overflow-x-hidden">
 				{#if $cart && $cart.lines?.nodes.length > 0}
 					<ul role="list" class="{cartIsUpdatingClass} gap-3">
 						{#each $cart.lines?.nodes as item, i}
@@ -149,56 +149,7 @@
 								</div>
 							</li>
 
-							<!-- <li class="grid py-8 grid-cols-12 gap-3">
-								<div class="overflow-clip rounded-lg col-span-3 lg:col-span-2">
-									<ShopifyImage
-										image={item.merchandise.image}
-										classList="object-cover h-full object-center aspect-1"
-										sizes="(min-width: 100px) 100px"
-										loading="lazy"
-									/>
-								</div>
-								<div class="col-span-7 lg:col-span-8 flex flex-col gap-2">
-									<a
-										class="hover:underline w-fit"
-										href={`/products/${item.merchandise.product.handle}`}
-									>
-										{item.merchandise.product.title}
-									</a>
-									<p class="text-xs">
-										<Money price={item.cost.amountPerQuantity} />
-									</p>
-								</div>
-								<div class="col-span-2 items-end flex justify-between flex-col">
-									<button
-										on:click={() => {
-											if ($isCartUpdating) return;
-											removeItem(item.id);
-										}}
-										class="hyper-button button-neutral flex items-center justify-center h-12 aspect-square font-suissnord"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											class="w-5 h-5"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-											/>
-										</svg>
-									</button>
-									<div>
-										<p class="font-medium">
-											<Money price={item.cost.totalAmount} />
-										</p>
-									</div>
-								</div>
-							</li> -->
+							
 						{/each}
 					</ul>
 				{:else}
