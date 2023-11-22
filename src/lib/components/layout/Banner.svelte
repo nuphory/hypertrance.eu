70c8976619ce24876d8631402f8894535aea1841
Mia <mia@mia.cx>
2023-11-22 23:11:03 +0100

    stupid frickin banner
    Signed-off-by: Mia <mia@mia.cx>

--- src/lib/components/layout/Banner.svelte
+++ src/lib/components/layout/Banner.svelte
@@ -1,3 +1,3 @@
-<div class="sticky top-0 z-50 flex justify-center items-center h-8 bg-[hsl(355_80%_55%)] text-white">
+<div class="fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-[hsl(355_80%_55%)] text-white">
         black friday sale — save 35%!
 </div>
