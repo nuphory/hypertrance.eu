be14cfd0f5055be54ab7df181c6db83fdb08129d
Mia <mia@mia.cx>
2023-11-28 21:47:43 +0100

    hidden banner no top padding header
    Signed-off-by: Mia <mia@mia.cx>

--- src/lib/components/layout/Banner.svelte
+++ src/lib/components/layout/Banner.svelte
@@ -1,3 +1,3 @@
-<div class="hidden fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-primary text-white"> <!-- bg-[hsl(355_80%_55%)] -->
+<div class="hidden fixed w-full top-0 z-50 justify-center items-center h-8 bg-primary text-white"> <!-- bg-[hsl(355_80%_55%)] -->
         Hey! you've found the "sale" banner! There's no sale active right now, but stay tuned for future ones!
 </div>
