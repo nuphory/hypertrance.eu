bd4c59a7dc031ff25a6f723ca70b968005aaa643
Mia <mia@mia.cx>
2023-11-28 21:43:39 +0100

    disable banner + handle discounts on front page
    Signed-off-by: Mia <mia@mia.cx>

--- src/lib/components/layout/Banner.svelte
+++ src/lib/components/layout/Banner.svelte
@@ -1,3 +1,3 @@
-<div class="fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-[hsl(355_80%_55%)] text-white">
-        black friday sale — save 35%!
+<div class="hidden fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-primary text-white"> <!-- bg-[hsl(355_80%_55%)] -->
+        Hey! you've found the "sale" banner! There's no sale active right now, but stay tuned for future ones!
 </div>
