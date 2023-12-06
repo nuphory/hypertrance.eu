f8749452a4a2bed62865bee1645f264d0a8c6856
Mia <mia@mia.cx>
2023-12-06 22:28:58 +0100

    banner thing
    Signed-off-by: Mia <mia@mia.cx>

--- src/lib/components/layout/Banner.svelte
+++ src/lib/components/layout/Banner.svelte
@@ -1,3 +1,3 @@
-<div class="hidden fixed w-full top-0 z-50 justify-center items-center h-8 bg-primary text-white"> <!-- bg-[hsl(355_80%_55%)] -->
-        Hey! you've found the "sale" banner! There's no sale active right now, but stay tuned for future ones!
+<div class="hidden fixed w-full top-0 z-50 justify-center items-center h-8 bg-primary bg-[hsl(355_80%_55%)] text-white"> <!-- bg-[hsl(355_80%_55%)] -->
+        Holiday Season — Save 35%
 </div>
