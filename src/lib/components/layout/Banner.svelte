5eb479ca7d3c4463a59df6d3b561e9819d193f01
Mia <mia@mia.cx>
2023-12-06 22:32:15 +0100

    christmas banner
    Signed-off-by: Mia <mia@mia.cx>

--- src/lib/components/layout/Banner.svelte
+++ src/lib/components/layout/Banner.svelte
@@ -1,3 +1,3 @@
-<div class="hidden fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-primary text-white"> <!-- bg-[hsl(355_80%_55%)] -->
-        Hey! you've found the "sale" banner! There's no sale active right now, but stay tuned for future ones!
+<div class="fixed w-full top-0 z-50 flex justify-center items-center h-8 bg-primary !bg-[hsl(120_100%_25%)] text-white"> <!-- bg-[hsl(355_80%_55%)] -->
+        <span>Holiday Season — Save 35%</span>
 </div>
