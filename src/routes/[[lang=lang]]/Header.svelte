7c8d30ac5c932dff5111a8fc53870db6424e64c6
default <mia@mia.cx>
2024-06-16 03:36:32 +0000

    header thing
    Signed-off-by: default <mia@mia.cx>

---
+++ src/routes/[[lang=lang]]/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header class="sticky">
+	<section class="absolute top-4 z-10">
+		<nav class="container">
+			<a href="/"
+				>{$page.url.hostname
+					.split('.')
+					.filter((v, i, a) => i >= a.length - 2)
+					.join('.')}</a
+			>
+			<ul></ul>
+		</nav>
+	</section>
+</header>
