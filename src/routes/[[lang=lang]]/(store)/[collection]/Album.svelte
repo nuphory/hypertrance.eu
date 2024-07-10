383b835995923daa62bc6d9711c95e7da3ac1e28
Mia <mia@mia.cx>
2024-07-10 16:48:57 +0200

    blank collection components
    Signed-off-by: Mia <mia@mia.cx>

---
+++ src/routes/[[lang=lang]]/(store)/[collection]/Album.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type Collection from '$lib/shopify/zod/catalog/collection/Collection';
+	import type { z } from 'zod';
+
+	let { collection }: { collection: z.infer<typeof Collection> } = $props();
+</script>
+
+<main>
+	<section id="hero">
+
+	</section>
+</main>
+
+<style>
+</style>
---
+++ src/routes/[[lang=lang]]/(store)/[collection]/Default.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type Collection from '$lib/shopify/zod/catalog/collection/Collection';
+	import type { z } from 'zod';
+
+	let { collection }: { collection: z.infer<typeof Collection> } = $props();
+</script>
+
+<h1>{collection.title}</h1>
+
+<style>
+</style>
