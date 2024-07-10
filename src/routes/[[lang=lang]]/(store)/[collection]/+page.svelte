3636fede74dbe78e8e292b204aa2126cf95e4e8c
Mia <mia@mia.cx>
2024-07-10 16:48:29 +0200

    logic to pick correct collection component
    Signed-off-by: Mia <mia@mia.cx>

---
+++ src/routes/[[lang=lang]]/(store)/[collection]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	import DefaultCollectionComponent from './Default.svelte';
+	import AlbumCollectionComponent from './Album.svelte';
+	import ArtbookCollectionComponent from './Artbook.svelte';
+
+	let { data }: { data: PageData } = $props();
+
+        const { collection } = data;
+        const {type} = collection;
+
+        const components: Record<string, any> = {
+                album: AlbumCollectionComponent,
+                artbook: ArtbookCollectionComponent,
+                collection: DefaultCollectionComponent
+        }
+
+        const CollectionComponent = components[type?.value ?? 'collection'];
+
+</script>
+
+<CollectionComponent {collection} />
