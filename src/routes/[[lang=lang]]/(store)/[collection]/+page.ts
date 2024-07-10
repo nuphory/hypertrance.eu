224db578119cedbcdf1a222573878a27edb67639
Mia <mia@mia.cx>
2024-07-10 16:48:11 +0200

    collection fetch (typed)
    Signed-off-by: Mia <mia@mia.cx>

--- src/routes/[[lang=lang]]/(store)/[collection]/+page.ts
+++ src/routes/[[lang=lang]]/(store)/[collection]/+page.ts
@@ -3,6 +3,7 @@ import client from '$lib/shopify/client';
 import Collection from '$lib/shopify/zod/catalog/collection/Collection';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import type { z } from 'zod';
 
 export const load: PageLoad = async ({ params }) => {
 	const { collection: handle } = params;
@@ -23,17 +24,21 @@ export const load: PageLoad = async ({ params }) => {
 
 	const { collection } = data;
 
-	if (!collection || collection === null /* || collection.vendor !== 'hypertrance' */) {
+	if (!collection || collection === null || collection.vendor?.value !== 'hypertrance') {
 		console.error(404, `Collection not found: ${handle}`);
 		error(404, { message: `Collection not found: ${handle}` });
 	}
 
-	// data.collection = Collection.parse(collection);
+	data.collection = Collection.parse(collection);
 
-	// console.info(200, 'OK', `Collection found: ${handle}`, data);
-
-	return {
-		...data,
-		extensions
+	const result: {
+		[key: string]: unknown;
+		collection: z.infer<typeof Collection>;
+	} = {
+		...data
 	};
+
+	console.info(200, 'OK', `Collection found: ${handle}`, result);
+
+	return result;
 };
