da4ff6634aee048c995a50d9c4e592a67bd6bff2
Mia <mia@mia.cx>
2024-07-10 13:57:38 +0200

    remove legacy
    Signed-off-by: Mia <mia@mia.cx>

--- src/routes/[[lang=lang]]/(index)/legacy/(assets)/music/player.ts
+++
@@ -1,4 +0,0 @@
-import { EuterpeBuilder } from '@euterpe.js/euterpe';
-import { make } from './db';
-export const player = new EuterpeBuilder(document.createElement('audio'), make(), { use_only_pathname_url: true }).build();
-
