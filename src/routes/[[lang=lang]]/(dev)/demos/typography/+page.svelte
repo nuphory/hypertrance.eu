cbb0db5e50eba21fded69a1cdd8508a65f85f129
Mia <mia@mia.cx>
2024-08-04 23:54:26 +0200

    move demo and replace it with placeholder
    Signed-off-by: Mia <mia@mia.cx>

---
+++ src/routes/[[lang=lang]]/(dev)/demos/typography/+page.svelte
@@ -0,0 +1,165 @@
+<script lang="ts">
+	import Link from '$lib/components/utils/Link.svelte';
+</script>
+
+<main>
+	<section id="hero" role="banner" class="py-32">
+		<div data-role="background" class="bg-dark-primary"></div>
+		<h1 class="text-hero">A Basic Scaffold</h1>
+		<span role="doc-subtitle" class="text-display"
+			>Welcome to this basic scaffold of an article document structure!</span
+		>
+
+		<ul>
+			<li>
+				<Link href="/">Home</Link>
+			</li>
+			<li>
+				<Link href="https://github.com/nuphory/hypertrance.eu">GitHub</Link>
+			</li>
+		</ul>
+
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti esse veritatis natus
+			tenetur, ab, laboriosam totam sequi quas tempore pariatur enim consectetur velit quod illo
+			commodi ipsa dolorem in molestias?
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, hic non? Minima sed quasi,
+			necessitatibus sit quia ratione. Cupiditate totam quae delectus ipsam illum corporis velit qui
+			commodi inventore magnam.
+		</p>
+	</section>
+
+	<section>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos esse tempore itaque, cum
+			rerum non officia ab. Perspiciatis, nobis sunt? Vero nulla perferendis fuga culpa magnam,
+			recusandae illo autem dolore!
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h1>primary heading</h1>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nisi sunt aliquid ea natus.
+			Repellendus, sed et quisquam reprehenderit asperiores hic laborum libero similique optio
+			ratione architecto, autem alias qui!
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h2>secondary heading</h2>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores adipisci veniam laboriosam
+			id, delectus repudiandae ipsa inventore quos in, odio omnis? Expedita architecto laboriosam
+			nulla nostrum est laborum quam voluptas?
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h3>tertiary heading</h3>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, minus est cumque iure animi
+			quas itaque eligendi officiis magnam incidunt rem numquam nemo sed iusto fugiat possimus quis
+			molestias asperiores.
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h4>quaternary heading</h4>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ratione itaque fugit.
+			Deleniti commodi nemo, eveniet repudiandae facilis magni blanditiis necessitatibus? Provident
+			asperiores expedita ullam quam saepe? Quod, recusandae officia?
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h5>quinary heading</h5>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita aliquid fugit cumque
+			reprehenderit quo sint, obcaecati natus! In dolorem, dolorum similique est nam corporis
+			aspernatur, veniam alias quidem, ducimus quis.
+		</p>
+		<span role="doc-subtitle">suptitle</span>
+		<h6>senary heading</h6>
+		<span role="doc-subtitle">subtitle</span>
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit reiciendis eos obcaecati
+			asperiores repellendus. Magnam, nemo maiores blanditiis voluptatum placeat ut saepe ab laborum
+			ex, cum repellendus adipisci aut rem?
+		</p>
+	</section>
+
+	<section class="bg-dark">
+		<h2 class="text-primary">This is a primary heading</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo corporis saepe praesentium
+			magnam, porro corrupti tenetur quas eum reprehenderit facere voluptate! Modi amet ipsa velit
+			in dolor ipsum magnam suscipit.
+		</p>
+
+		<h2>Some more information</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo et quos est alias commodi
+			corporis, libero repudiandae eius sed ullam consequatur, magni rem, repellendus nobis sit
+			dolores quod blanditiis molestias!
+		</p>
+	</section>
+
+	<section class="bg-dark-primary-island">
+		<h2>Another section</h2>
+		<span role="doc-subtitle">This guy even has a subtitle</span>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, aliquid necessitatibus.
+			Praesentium eius itaque corrupti odio corporis, alias et tenetur nihil repellendus esse
+			veritatis maiores amet est debitis fugiat suscipit!
+		</p>
+
+		<span role="doc-subtitle" class="text-overline">Some random overline</span>
+		<h3>And a tertiary heading?</h3>
+		<span role="doc-subtitle" class="text-overline"><a href="/">a clickable underline?</a></span>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non blanditiis ea sapiente, vel
+			neque sit aliquid quis. Eveniet beatae commodi aperiam labore, provident voluptate iste quasi.
+			Officia vel quam itaque.
+		</p>
+	</section>
+
+	<section>
+		<span role="doc-subtitle">A suptitle is a subtitle that is placed above its heading</span>
+		<h2>Look at me! I have a suptitle!</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum repudiandae odit facere,
+			dolorem esse ducimus ullam placeat quidem quas id perferendis labore voluptates. Impedit
+			magnam quibusdam rerum iure esse ipsa.
+		</p>
+	</section>
+
+	<section>
+		<div data-role="background" class="bg-dark-primary"></div>
+		<h3>A level 3 heading with a subtitle</h3>
+		<span role="doc-subtitle">Just the subtitle I was looking for.</span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit error eligendi autem
+			illo accusantium in, commodi repellat id molestiae mollitia pariatur ipsum fugiat beatae
+			laborum laudantium delectus dolores! Qui, quo.
+		</p>
+		<h4>Now we're getting a little crazy with a level 4 heading</h4>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis quibusdam, veritatis recusandae
+			ipsam vero corrupti rerum perspiciatis laborum deleniti fugiat in. Maxime officia adipisci
+			maiores. Voluptate vitae quibusdam laboriosam dolorum.
+		</p>
+		<h5>h5 has the same font-size as an h4</h5>
+		<h6>And so does h6</h6>
+		<span role="doc-subtitle"> You may also have noticed, that h4-6 are bold! </span>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illo ipsam dolorem cupiditate
+			labore temporibus quas illum, quasi aspernatur vero iste ducimus quae, quaerat quam
+			recusandae, tenetur enim mollitia ullam?
+		</p>
+	</section>
+
+	<section>
+		<a href="/">This is a link</a>
+
+		<button>This is a button</button>
+	</section>
+</main>
